4b3629ec9ce77df83684b6b98bb1e8c0fb882a0c
gorjanmicevski <micevskigorjan2001@gmail.com>
2022-04-13 15:08:02 +0200

    initial

--- front-end/album-collector/src/app/stickers-grid/stickers-grid.component.ts
+++ front-end/album-collector/src/app/stickers-grid/stickers-grid.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stickers-grid',
   templateUrl: './stickers-grid.component.html',
-  styleUrls: ['./stickers-grid.component.css']
+  styleUrls: ['./stickers-grid.component.css'],
 })
 export class StickersGridComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  rowRange = new Array(20);
+  colRange = new Array(20);
+  ngOnInit(): void {}
 }
