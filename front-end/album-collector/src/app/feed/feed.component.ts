eee234ddd6040316e2243eff5591c4ff0afda2ba
gorjanmicevski <micevskigorjan2001@gmail.com>
2022-04-19 22:34:32 +0200

    filtering

--- front-end/album-collector/src/app/feed/feed.component.ts
+++ front-end/album-collector/src/app/feed/feed.component.ts
@@ -9,9 +9,96 @@ import { FeedService } from '../feed.service';
 export class FeedComponent implements OnInit {
   constructor(private feedService: FeedService) {}
   popup = false;
+<<<<<<< Updated upstream
+=======
+  allposts = [
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+    {
+      username: 'Gorjan',
+      dateTime: '40 minutes ago',
+      missingCards: 10,
+      duplicateCards: 100,
+      phone: '070238300',
+      place: 'rekord',
+    },
+  ];
+  posts: {
+    username: string;
+    dateTime: string;
+    missingCards: number;
+    duplicateCards: number;
+    phone: string;
+    place: string;
+  }[] = [];
+  page = 1;
+>>>>>>> Stashed changes
   ngOnInit(): void {
     // this.feedService.getPosts().subscribe((data) => console.log(data));
     // this.feedService.addPost({ test: 'test' });
+    this.posts = [...this.posts, ...this.getNewPage(this.page)];
   }
   addPost() {
     console.log('post');
--- front-end/album-collector/src/app/stickers-grid/stickers-grid.component.ts
+++ front-end/album-collector/src/app/stickers-grid/stickers-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of, tap } from 'rxjs';
+import { FeedService } from '../feed.service';
 
 @Component({
   selector: 'app-stickers-grid',
@@ -6,8 +8,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stickers-grid.component.css'],
 })
 export class StickersGridComponent implements OnInit {
-  constructor() {}
-  rowRange = new Array(20);
+  constructor(private feedService: FeedService) {}
+  rowRange = new Array(0);
   colRange = new Array(20);
+<<<<<<< Updated upstream
   ngOnInit(): void {}
+=======
+  stickers: any[] | undefined;
+  allStickers: any[] | undefined;
+  test = 'All';
+  ngOnInit(): void {
+    this.feedService.getStickers().subscribe((data) => {
+      this.allStickers = data;
+      this.allStickers = this.allStickers.map((sticker) => {
+        return { ...sticker, stickerClass: 'missing' };
+      });
+      this.stickers = [...this.allStickers];
+      console.log(this.stickers);
+      this.calculateRowRange();
+    });
+  }
+  calculateRowRange() {
+    this.rowRange = new Array(Math.ceil(this.stickers?.length! / 20));
+    console.log(this.rowRange.length);
+  }
+  inColRange(index: number) {
+    if (this.stickers!.length <= index) return false;
+    return true;
+  }
+  stickerClass = 'missing';
+  selected = 'All';
+  alterSticker(index: number) {
+    switch (this.stickers![index].stickerClass) {
+      case 'missing':
+        this.stickers![index].stickerClass = 'collected';
+
+        break;
+      case 'collected':
+        this.stickers![index].stickerClass = 'duplicate';
+        break;
+      case 'duplicate':
+        this.stickers![index].stickerClass = 'missing';
+        break;
+    }
+  }
+  filterStickers(option: any) {
+    this.selected = option;
+    switch (option) {
+      case 'All':
+        this.stickers = this.allStickers?.map((s) => s);
+        break;
+      case 'Missing':
+        this.stickers = this.allStickers?.filter(
+          (s) => s.stickerClass == 'missing'
+        );
+        break;
+      case 'Duplicates':
+        this.stickers = this.allStickers?.filter(
+          (s) => s.stickerClass == 'duplicate'
+        );
+        break;
+      case 'Collected':
+        this.stickers = this.allStickers?.filter(
+          (s) => s.stickerClass == 'collected'
+        );
+        break;
+    }
+
+    console.log(option);
+  }
+>>>>>>> Stashed changes
 }
